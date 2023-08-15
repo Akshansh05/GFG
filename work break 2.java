add3f5c545405f927336ae25a55b677149ed364e
Akshansh Ohm <akshanshohm1999@gmail.com>
2023-08-15 23:37:28 +0530

    Update work break 2.java

--- work break 2.java
+++ work break 2.java
@@ -1,49 +1,25 @@
-int dictionaryContains(string &word)
-{
-    string dictionary[] = {"mobile","samsung","sam","sung",
-                            "man","mango", "icecream","and",
-                            "go","i","love","ice","cream"};
-    int n = sizeof(dictionary)/sizeof(dictionary[0]);
-    for (int i = 0; i < n; i++)
-        if (dictionary[i].compare(word) == 0)
-            return true;
-    return false;
-}
- 
-// Prints all possible word breaks of given string
-void wordBreak(string str)
-{
-    // Last argument is prefix
-    wordBreakUtil(str, str.size(), "");
-}
- 
-// Result store the current prefix with spaces
-// between words
-void wordBreakUtil(string str, int n, string result)
-{
-    //Process all prefixes one by one
-    for (int i=1; i<=n; i++)
-    {
-        // Extract substring from 0 to i in prefix
-        string prefix = str.substr(0, i);
- 
-        // If dictionary contains this prefix, then
-        // we check for remaining string. Otherwise
-        // we ignore this prefix (there is no else for
-        // this if) and try next
-        if (dictionaryContains(prefix))
-        {
-            // If no more elements are there, print it
-            if (i == n)
-            {
-                // Add this element to previous prefix
-                result += prefix;
-                cout << result << endl;
-                return;
-            }
-            wordBreakUtil(str.substr(i, n-i), n-i,
-                                result + prefix + " ");
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String>  ans = new ArrayList<>();
+            helper(s,s.length(),"",ans,wordDict);
+        return ans;
+    }
+
+     void helper(String s , int n ,String result , List<String> ans,List<String> wordDict){
+        
+        for(int i =1; i<=n;i++){
+
+            String prefix = s.substring(0,i);
+
+            if(wordDict.contains(prefix)){
+                   if(i==n){ 
+                    result += prefix;
+                    ans.add(result);
+                    return;
+                }
+           
+             helper(s.substring(i ,n),n-i,result+prefix+ " ",ans ,wordDict) ;
+             }
         }
-    }     
+    }
 }
- 
