db52e660d5d08db90a3cf6c3620b90041806d264
Akshansh Ohm <akshanshohm1999@gmail.com>
2023-08-21 16:06:17 +0530

    Create  Target Subset sum .java

---
+++ Target Subset sum .java
@@ -0,0 +1,26 @@
+boolean subsetSum(int[] nums , int index , int sum, int dp[][]){
+        if(index == 0){
+            if(sum == nums[0]){
+                return true;
+            }
+            return false;
+        }
+        if(dp[index][sum]!=-1){
+            return dp[index][sum] == 0 ? true:false;
+        }
+        boolean take =false;
+        if(nums[index] <= sum){
+             take = subsetSum(nums,index-1 , sum-nums[index] ,dp);
+        }
+          boolean notTake = subsetSum(nums,index-1 , sum ,dp);
+
+            boolean  val = take || notTake;
+
+            dp[index][sum] =  val == true ? 0:1;
+
+          return val;
+    
+    }
+
+
+//see if the subset and get the target sum or not 
