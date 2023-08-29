d2cc0d311d71d536a92e7f7c07aea14adf979724
Akshansh Ohm <akshanshohm1999@gmail.com>
2023-08-29 10:22:01 +0530

    Update Shortest path in Undirected Graph having unit distance.java

--- Shortest path in Undirected Graph having unit distance.java
+++ Shortest path in Undirected Graph having unit distance.java
@@ -5,32 +5,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            int m=sc.nextInt();
-            int[][] edge = new int[m][2];
-            for(int i=0;i<m;i++){
-                edge[i][0]=sc.nextInt();
-                edge[i][1]=sc.nextInt();
-            }
-            int src=sc.nextInt();
-            Solution obj = new Solution();
-            int res[] = obj.shortestPath(edge,n,m,src);
-            for(int i=0;i<n;i++){
-                System.out.print(res[i]+" ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     
     public int[] shortestPath(int[][] edges,int N,int M ,int src) {
