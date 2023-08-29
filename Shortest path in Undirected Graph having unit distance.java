c586726f715a09d79db8ec7f68af2631cb69e4c4
Akshansh Ohm <akshanshohm1999@gmail.com>
2023-08-29 10:21:33 +0530

    Update Shortest path in Undirected Graph having unit distance.java

--- Shortest path in Undirected Graph having unit distance.java
+++ Shortest path in Undirected Graph having unit distance.java
@@ -1,100 +1,85 @@
 Shortest path in Undirected Graph having unit distance  BFS
 
+	//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
 
 class Solution {
+    
+    public int[] shortestPath(int[][] edges,int N,int M ,int src) {
+        // Code here
+        
+        int distance[] = new int[N];
 
-	public int[] shortestPath(int N,int M, int[][] edges) {
+		 List<List<Integer>> adj = new ArrayList<>();
 	
-		//Code here
-		
-		//steps  1.do topo sort 
-		//2.   do      distance[child.child] = Math.min(distance[child.child],distanceOfSourceToCurrToChild);
 
-		
-		int inorder[] = new int[N];
-		 int distance[] = new int[N];
-		
-		 List<List<Node>> adj = new ArrayList<>();
-		
-		for(int i =0 ;i <N ;i++){
-		    inorder[i] = 0;
+        for(int i =0 ;i <N ;i++){
 		    distance[i] = (int)Math.pow(10,9);
-		    adj.add(new ArrayList<Node>());
+		    adj.add(new ArrayList<Integer>());
 		}
 		
-
+		
     for(int i=0;i <M;i++){
         int par = edges[i][0];
         int child = edges[i][1];
-        int weight = edges[i][2];
-        
-        Node n = new Node(child,weight);
-        
-        adj.get(par).add(n);
-        
-        inorder[child]++;
+        adj.get(par).add(child);//undirected graph 
+         adj.get(child).add(par);//undirected graph 
         
     }
-    
-    Queue<Integer> q = new LinkedList<>();
-    
-    
-           for(int i=0;i<N;i++){
-           if(inorder[i] ==0 ){
-               q.add(i); 
-           }
-       }
-       
-       List<Integer> topo = new ArrayList<>();
-       
-       while(!q.isEmpty()){
-           
-           int curr = q.poll();
-           
-           topo.add(curr);
-           
-            for(Node val : adj.get(curr)){
-               inorder[val.child]--; 
-               if(inorder[val.child] ==0){
-                   q.add(val.child); 
-               }
-           }
-       }
-       
-    
-       distance[0] = 0; // src distance as 0 
-       
-       for(int i =0;i < topo.size();i++){
-           
-           int curr = topo.get(i);
-           
-           for(Node child : adj.get(curr)){
-               
-               int distanceOfSourceToCurrToChild =   distance[curr] + child.weight; 
-               
-              distance[child.child] = Math.min(distance[child.child],distanceOfSourceToCurrToChild);
-              
-           }
-       }
-       
-       for(int i=0; i< N;i++){
+		
+		distance[src] = 0;
+		
+		 Queue<Integer> q = new LinkedList<>();
+		 q.add(src);
+		 
+        while(!q.isEmpty()){
+            
+            int curr  = q.remove();
+            
+            for(int i= 0;i < adj.get(curr).size();i++){
+                if(distance[adj.get(curr).get(i)] == (int)Math.pow(10,9) ){ //if unvisited 
+          int distanceOfSourceToCurrToChild =   distance[curr] +  1; 
+            distance[adj.get(curr).get(i)] =   Math.min(distance[adj.get(curr).get(i)],distanceOfSourceToCurrToChild);
+                q.add(adj.get(curr).get(i));
+                }
+            }
+        }
+         for(int i=0; i< N;i++){
            
            if(distance[i] == (int)Math.pow(10,9)){
               distance[i] = -1; 
            }
        }
-       
-       return distance;
+        
+        return distance;
 		
-		
-	}
-	
-	class Node{
-	    int child ;
-	    int weight;
-	    public Node(int child ,int weight){
-	        this.child = child;
-	        this.weight = weight;
-	    }
-	}
+    }
 }
